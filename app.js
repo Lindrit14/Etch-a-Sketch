678462254f40953dc142ca1b3ccb7c860a6f62ac
Lindrit14 <Lindrit_lindi@hotmail.com>
2022-02-08 19:06:10 +0100

    add the logic of changing the backgroundColor of the divs and add make the color change bades on an UI

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const mainContainer = document.querySelector(".mainContainer");
 const tempButtonAdd = document.querySelector(".add");
 const tempButtonRemove = document.querySelector(".remove");
-
+const buttonChangeBackground = document.querySelector(".changeBackground")
+const colorPicker = document.getElementById("colorPicker");
 
 let userInput = parseInt(prompt("How many divs?"));
 function addDiv(){
@@ -15,11 +16,8 @@ function addDiv(){
             newDiv.className = "pixel";
             mainContainer.appendChild(newDiv);
             console.log("Yo");
-    
+        }
     }
-    
-    }
-    
 }
 
 function removeDiv(){
@@ -28,10 +26,27 @@ function removeDiv(){
     newDiv.forEach((div) => {
         div.remove();
     })
-    
-     
 }
+
+function changeBackground(){
+    const newDiv = document.querySelectorAll(".pixel");
+
+    newDiv.forEach((div)=>{
+        div.style.backgroundColor=colorPicked;
+})
+}
+let colorPicked;
+
+colorPicker.addEventListener("input", (e)=>{
+    colorPicked = e.target.value;
+})
+
+
 
 tempButtonAdd.addEventListener("click", addDiv);
 
 tempButtonRemove.addEventListener("click", removeDiv);
+
+buttonChangeBackground.addEventListener("click", changeBackground);
+
+
