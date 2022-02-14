10e1878f9798c2a59b6ed0089c94911317eab3bd
Lindrit14 <Lindrit_lindi@hotmail.com>
2022-02-14 19:26:35 +0100

    make function that renders the 16x16 Grid when the site loads in, style the buttons and the layout of the site

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
+let colorPicked;
+let divPerRowGrid = 16; 
+const widthMainContainer = document.getElementById("propertyMainContainer").clientWidth;
+const heightMainContainer = document.getElementById("propertyMainContainer").clientHeight;
+
 const mainContainer = document.querySelector(".mainContainer");
 const tempButtonAdd = document.querySelector(".add");
 const tempButtonRemove = document.querySelector(".remove");
 const buttonChangeBackground = document.querySelector(".changeBackground")
 const colorPicker = document.getElementById("colorPicker");
 
-let userInput = parseInt(prompt("How many divs?"));
-function addDiv(){
 
-    if(userInput >=101){
-        alert("To big of a number");
-    }else{
-        userInput *= userInput; 
-        for(i=0;i<userInput;i++){
-            const newDiv = document.createElement("div");
-            newDiv.className = "pixel";
-            mainContainer.appendChild(newDiv);
-            console.log("Yo");
-        }
-    }
-}
+
+function renderFirstTimeGrid(){
+    for(i=1;i<=divPerRowGrid*divPerRowGrid;i++){
+        const newDiv = document.createElement("div");
+        newDiv.classList = "pixel";
+        newDiv.style.width = (widthMainContainer / divPerRowGrid) + "px";
+        newDiv.style.height = (widthMainContainer / divPerRowGrid) + "px";
+        mainContainer.appendChild(newDiv);
+    };
+    console.log("Grid rendered for the first time");
+};
+
+
+
+
 
 function removeDiv(){
     const newDiv = document.querySelectorAll(".pixel");
@@ -35,15 +41,14 @@ function changeBackground(){
         div.style.backgroundColor=colorPicked;
 })
 }
-let colorPicked;
 
 colorPicker.addEventListener("input", (e)=>{
     colorPicked = e.target.value;
 })
 
 
+window.onload = renderFirstTimeGrid();
 
-tempButtonAdd.addEventListener("click", addDiv);
 
 tempButtonRemove.addEventListener("click", removeDiv);
 
