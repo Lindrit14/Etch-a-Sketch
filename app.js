82cf0d1aeb586f5366d7007a77b9d42b5bc92edd
Lindrit14 <Lindrit_lindi@hotmail.com>
2022-02-15 19:12:51 +0100

    add the functionality of making a new grid with an input range element, add functionality of changing the color of the divs

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-let colorPicked;
+let colorPicked = "#000000";
 let divPerRowGrid = 16; 
-const widthMainContainer = document.getElementById("propertyMainContainer").clientWidth;
-const heightMainContainer = document.getElementById("propertyMainContainer").clientHeight;
+
 
 const mainContainer = document.querySelector(".mainContainer");
-const tempButtonAdd = document.querySelector(".add");
-const tempButtonRemove = document.querySelector(".remove");
 const buttonChangeBackground = document.querySelector(".changeBackground")
 const colorPicker = document.getElementById("colorPicker");
-
+const widthMainContainer = document.getElementById("propertyMainContainer").clientWidth;
+const gridRange = document.querySelector("#gridRange");
 
 
 function renderFirstTimeGrid(){
@@ -18,40 +16,60 @@ function renderFirstTimeGrid(){
         newDiv.style.width = (widthMainContainer / divPerRowGrid) + "px";
         newDiv.style.height = (widthMainContainer / divPerRowGrid) + "px";
         mainContainer.appendChild(newDiv);
-    };
+    }
     console.log("Grid rendered for the first time");
 };
 
-
-
-
-
-function removeDiv(){
+function changeColor(){
     const newDiv = document.querySelectorAll(".pixel");
-    
     newDiv.forEach((div) => {
-        div.remove();
+        div.addEventListener("mousedown", (e)=>{
+            div.style.backgroundColor = colorPicked;
+            console.log(e.target);
+        })
+        div.addEventListener("mousemove", ()=>{
+            div.style.backgroundColor = colorPicked;
+            console.log()
+        })
+      
     })
-}
+};
 
-function changeBackground(){
+function removeGrid(){
     const newDiv = document.querySelectorAll(".pixel");
-
     newDiv.forEach((div)=>{
-        div.style.backgroundColor=colorPicked;
-})
+        div.remove(); 
+    })
+};
+
+function renderNewGrid(){
+    for(i=1;i<=divPerRowGrid*divPerRowGrid;i++){
+        const newDiv = document.createElement("div");
+        newDiv.classList = "pixel";
+        newDiv.style.width = (widthMainContainer / divPerRowGrid) + "px";
+        newDiv.style.height = (widthMainContainer / divPerRowGrid) + "px";
+        mainContainer.appendChild(newDiv);
+    }
 }
 
 colorPicker.addEventListener("input", (e)=>{
     colorPicked = e.target.value;
-})
+    console.log(colorPicked);
+});
 
 
+
+gridRange.addEventListener("click", (e)=>{
+    divPerRowGrid = e.target.value;
+    removeGrid();
+    renderNewGrid(); 
+    changeColor();
+    console.log(e.target.value)
+});
 window.onload = renderFirstTimeGrid();
+window.onload = changeColor();
 
 
-tempButtonRemove.addEventListener("click", removeDiv);
 
-buttonChangeBackground.addEventListener("click", changeBackground);
 
 
